b99640b6620e8a38129e2f27de13e2ba59956acf
jwthomas44 <helpedpig831@gmail.com>
2026-03-03 10:49:38 -0600

    added needed endpoints

--- app.js
+++ app.js
@@ -16,32 +16,134 @@ app.get("/web/ping", (req, res) => {
 });
 
 app.get("/web/nodes", (req, res) => {
-  const nodeText = `{
-    "nodes": [
-      {
-        "id": "node001",
-        "last_seen": "3456789"
-      },
-      {
-        "id": "node002",
-        "last_seen": "3456746"
-      },
-      {
-        "id": "node003",
-        "last_seen": "4386952"
-      },
-      {
-        "id": "node004",
-        "last_seen": "6538205"
-      }
+  const nodeText = `[
+  {
+    "id": 0,
+    "name": "Viewing Node",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 97.12 }
     ]
-  }`;
+  },
+  {
+    "id": 1,
+    "name": "Front Gate",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 97.12 },
+      { "type": "door", "value": "Open" }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Back Gate",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 87.12 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 3,
+    "name": "Barn Door 1",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 39.95 },
+      { "type": "door", "value": "Open" }
+    ]
+  },{
+    "id": 4,
+    "name": "Barn Door 2",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 100.00 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 5,
+    "name": "Cave Entrance",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 60.01 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 6,
+    "name": "Secret Door",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 33.95 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 7,
+    "name": "Bat Cave",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 53.95 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 8,
+    "name": "Trapdoor",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 30.95 },
+      { "type": "door", "value": "Closed" }
+    ]
+  },{
+    "id": 9,
+    "name": "Reliquary Tower",
+    "type": "sensor",
+    "status": "Online",
+    "sensors": [
+      { "type": "battery", "value": 80.95 },
+      { "type": "door", "value": "Open" }
+    ]
+  }
+]`;
 
   console.log(`Sending: \n ${nodeText}`);
 
   nodeJSON = JSON.parse(nodeText);
 
   res.json(nodeJSON);
+});
+
+app.get("/web/alerts", (req, res) => {
+  const alertText = `[
+  {
+    "id": 1,
+    "name": "Front Gate",
+    "time": 154200,
+    "reasons": ["Door Opened", "Low Battery"]
+  },
+  {
+    "id": 3,
+    "name": "Barn Door 1",
+    "time": 154500,
+    "reasons": ["Door Opened", "Low Battery"]
+  },
+  {
+    "id": 9,
+    "name": "Reliquary Tower",
+    "time": 154800,
+    "reasons": ["Door Opened", "Low Battery"]
+  }
+]`;
+
+  const alertJSON = JSON.parse(alertText);
+
+  res.json(alertJSON);
+  console.log(`Sent alert: \n ${alertText}`);
 });
 
 app.get("/web/node", (req, res) => {
